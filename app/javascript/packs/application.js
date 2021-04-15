f8b449546b59285d70dd70be005b84098464aeb1
Marnoel de Guzman <marnoeldeguzman@gmail.com>
2021-04-15 14:50:08 +0800

    Added Model and Views on Devise

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+global.toastr = require("toastr")
+import "../stylesheets/application"
 
 Rails.start()
 Turbolinks.start()
