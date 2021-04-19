07939c674c99bd4e7611d5a58c6a63f006969c18
edisaurus <edswork.ece@gmail.com>
2021-04-19 15:40:22 +0800

    add roles variables at line 7

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
-  has_many :transactions
   rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  ROLES = %w[admin broker buyer].freeze
+  has_many :transactions
 end
