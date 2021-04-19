bc5a3136d965b93f105bec3f1442d2935e617ac1
edisaurus <edswork.ece@gmail.com>
2021-04-19 15:38:47 +0800

    add code at line 4 & after_sign_in_path_for at line 12

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,23 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception
-    before_action :authenticate_user!  
+  protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+#   def configure_permitted_parameters
+#     devise_parameter_sanitizer
+#   end
+
+  def after_sign_in_path_for(resource)
+    if current_user.has_role? :admin
+      dashboard_path
+    elsif current_user.has_role? :buyer
+      buyers_path
+    elsif current_user.has_role? :broker
+      brokers_path
+    else
+      root_path
+    end
+  end
 end
