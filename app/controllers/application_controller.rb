7aa8b4e845a858262340dbd6b23d564b55a8cb57
edisaurus <edswork.ece@gmail.com>
2021-04-20 16:28:32 +0800

    refactor dashboard path for admin role

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,24 +5,23 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:email,
-                                                      :password])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email,
-                                                       :password,
-                                                       :password_confirmation],
-                                                       role: [])
-  end
-
-  def after_sign_in_path_for(resource)
-    if current_user.has_role? :admin
-      dashboard_path
-    elsif current_user.has_role? :buyer
-      buyers_path
-    elsif current_user.has_role? :broker
-      brokers_path
-    else
-      root_path
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_in, keys: [:email,
+                                                         :password])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:email,
+                                                         :password,
+                                                         :password_confirmation])
     end
-  end
+
+    def after_sign_in_path_for(resource)
+      if current_user.has_role? :admin
+        rails_admin_path
+      elsif current_user.has_role? :buyer
+        buyers_path
+      elsif current_user.has_role? :broker
+        brokers_path
+      else
+        root_path
+      end
+    end
 end
