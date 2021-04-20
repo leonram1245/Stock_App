9a48c5438445955c1ea975a4381efe70e2ccdb46
edisaurus <edswork.ece@gmail.com>
2021-04-20 16:31:25 +0800

    fix spacing from line 6 to 18

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,17 +4,16 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-      user ||= User.new 
-      if user.has_role? :admin
-        can :manage, :all
-        can :access, :rails_admin       # only allow admin users to access Rails Admin
-        can :manage, :dashboard         # allow access to dashboard
-      elsif user.has_role? :buyer
-        can :manage, :all
-      elsif user.has_role? :broker
-        can :manage, :all
-        can :read, :all
-      end
-    
+    user ||= User.new 
+    if user.has_role? :admin
+      can :manage, :all
+      can :access, :rails_admin       # only allow admin users to access Rails Admin
+      can :manage, :dashboard         # allow access to dashboard
+    elsif user.has_role? :buyer
+      can :manage, :all
+    elsif user.has_role? :broker
+      can :manage, :all
+      can :read, :all
+    end
   end
 end
