1272c4292849aa1081118392a0babf30f16a8c0b
edisaurus <edswork.ece@gmail.com>
2021-04-21 14:43:29 +0800

    set resources for buyers stock & stock

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
  
- 
   devise_for :users
   root 'home#index'
   get 'home/index'
 
-  resources :brokers
-  resources :buyers
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :buyers_stocks
+  resources :stocks
 end
