ec14baf8bb9e0b1abf93c972d42fd04930207a7e
edisaurus <edswork.ece@gmail.com>
2021-04-21 08:06:28 +0800

    configure admin & buyer feature

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,11 +6,12 @@ class Ability
   def initialize(user)
     user ||= User.new 
     if user.has_role? :admin
-      can :manage, :all
+      can :manage, User
+      can :read, Transaction
       can :access, :rails_admin       # only allow admin users to access Rails Admin
       can :manage, :dashboard         # allow access to dashboard
     elsif user.has_role? :buyer
-      can :manage, :all
+      can :manage, Buyers_stock
     elsif user.has_role? :broker
       can :manage, :all
       can :read, :all
