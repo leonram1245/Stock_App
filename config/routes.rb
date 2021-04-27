cab2bc7e80653af665e1acc8f3d9a02004c2d8e8
edisaurus <edswork.ece@gmail.com>
2021-04-27 11:33:20 +0800

    no changes done

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
-  get 'dashboard/index'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  resources :dashboard, only: [:index]
+  
   devise_for :users, controllers:  {
     confirmations:'confirmations',
     registrations: 'registrations' 
   }
+  
   root 'home#index'
   get 'home/index'
+  get 'stocks/dashboard'
+  get 'buyers_stocks/dashboard'
 
-  
   resources :deals do
     resources :buyers_stocks
     resources :stocks
