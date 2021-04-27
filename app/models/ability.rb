6aa11c31c4fd46bee8c5da40aa4659ee1d5b1ac6
edisaurus <edswork.ece@gmail.com>
2021-04-27 11:36:30 +0800

    set buyer & broker ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -11,10 +11,8 @@ class Ability
       can :manage, :dashboard         # allow access to dashboard
     elsif user.has_role? :buyer
       can :manage, Buyers_stock
-      can :read, Buyers_stock
     elsif user.has_role? :broker
       can :manage, Stock
-      can :read, Stock
     end
   end
 end
