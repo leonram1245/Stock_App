523aa249fa978bad3bf3b6fefa24caf1738492da
Marnoel de Guzman <marnoeldeguzman@gmail.com>
2021-04-30 03:13:50 +0800

    added associations on stock model

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,5 +1,7 @@
 class Stock < ApplicationRecord
   resourcify
-  belongs_to :deal
+  belongs_to :user
+  has_many :broker_stocks, dependent: :destroy
+  has_many :buyers_stocks, dependent: :destroy
   validates :price, :ticker, :company, presence: true
 end
