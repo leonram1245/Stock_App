9342f95b6fc81bda79dc3d80b775f5466874d251
Marnoel de Guzman <marnoeldeguzman@gmail.com>
2021-04-30 18:58:29 +0800

    change routes and resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'broker_transactions/index'
+  get 'buyers_transactions/index'
+  get 'broker_stocks/index'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   
   devise_for :users, controllers:  {
@@ -10,8 +13,14 @@ Rails.application.routes.draw do
   get 'home/index'
 
   
-  resources :deals do
-    resources :buyers_stocks
-    resources :stocks
-  end
+
+resources :broker_stocks do
+  resources :broker_transactions 
 end
+resources :buyers_stocks do
+  resources :buyers_transactions
+end
+end
+
+
+  
