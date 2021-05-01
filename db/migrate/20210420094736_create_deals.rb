2e35433bbd87168e1037f581b119a02d162f6a22
edisaurus <edswork.ece@gmail.com>
2021-05-01 14:32:19 +0800

    merge schema changes

--- db/migrate/20210420094736_create_deals.rb
+++ db/migrate/20210420094736_create_deals.rb
@@ -1,9 +1,9 @@
 class CreateDeals < ActiveRecord::Migration[6.1]
   def change
-    create_table :deals do |t|
-      t.integer :user_id
+    # create_table :deals do |t|
+    #   t.integer :user_id
 
-      t.timestamps
-    end
+    #   t.timestamps
+    # end
   end
 end
