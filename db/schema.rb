3fdb7a2408c8fdc17e38f57217b2fd404e3fad1c
edisaurus <edswork.ece@gmail.com>
2021-05-01 15:06:58 +0800

    merge & update schema on alpha branch

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_30_061424) do
+ActiveRecord::Schema.define(version: 2021_04_30_105140) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "broker_stocks", force: :cascade do |t|
-    t.integer "stock_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.integer "quantity"
+    t.string "company"
+    t.decimal "price"
+    t.string "ticker"
+    t.decimal "amount"
   end
 
   create_table "broker_transactions", force: :cascade do |t|
@@ -58,15 +63,6 @@ ActiveRecord::Schema.define(version: 2021_04_30_061424) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
     t.index ["resource_type", "resource_id"], name: "index_roles_on_resource"
-  end
-
-  create_table "stocks", force: :cascade do |t|
-    t.float "price"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "ticker"
-    t.string "company"
-    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
