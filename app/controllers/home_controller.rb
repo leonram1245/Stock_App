9dc1f4e6c3f2747a20fcfe389a8746dbf372206c
edisaurus <edswork.ece@gmail.com>
2021-05-03 01:08:28 +0800

    add iex client at search page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,6 +6,13 @@ class HomeController < ApplicationController
     )
   end
 
+  def search
+    @client = IEX::Api::Client.new(
+      publishable_token: 'Tpk_5a1173a0676d48fd8e83ac1798fd8669',
+      endpoint: 'https://sandbox.iexapis.com/v1'
+    )
+  end
+
   def about
   end
 end
