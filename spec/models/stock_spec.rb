bfae670ca2d536f8e6692761b9d6610b697366c7
edisaurus <edswork.ece@gmail.com>
2021-05-02 23:40:47 +0800

    remove old model test file

--- spec/models/stock_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Stock, type: :model do
-  it { expect(described_class.new).to validate_presence_of :price }
-  it { expect(described_class.new).to validate_presence_of :ticker }
-  it { expect(described_class.new).to validate_presence_of :company }
-end
