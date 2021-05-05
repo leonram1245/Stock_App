21b5aa14795dd1734e2393c6d22a3c308a372358
Marnoel de Guzman <marnoeldeguzman@gmail.com>
2021-05-05 11:13:48 +0800

    add cancan rescue handler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  
+   rescue_from CanCan::AccessDenied do |exception|
+    respond_to do |format|
+      format.json { render nothing: true, status: :not_found }
+      format.html { redirect_to destroy_user_session_path, method: :delete, notice: exception.message, status: :not_found }
+      format.js   { render nothing: true, status: :not_found }
+    end
+  end
 
   protected
-
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_in, keys: [:email,
                                                          :password])                                            
