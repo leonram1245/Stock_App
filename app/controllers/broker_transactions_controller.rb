704660e2ca57a5b721258c036f415501390acef5
edisaurus <edswork.ece@gmail.com>
2021-05-05 08:34:50 +0800

    [WIP] refactor strong params and methods

--- app/controllers/broker_transactions_controller.rb
+++ app/controllers/broker_transactions_controller.rb
@@ -1,14 +1,21 @@
 class BrokerTransactionsController < ApplicationController
+  # before_action :set_broker_transaction, only: %i[ show edit update destroy ]
+  # before_action :get_broker_stock
+  
   def index
-    @broker_transactions = BrokerStock.broker_transactions
+     @broker_transactions = BrokerTransaction.all
   end
 
   private
-    def set_broker_stock
-      @broker_stock = current_user.broker_stocks.find(params[:id])
-    end
+    # def get_broker_stock
+    #   @broker_stock = current_user.broker_stocks.find(params[:broker_stock_id])
+    # end
 
-    def broker_stock_params
-      params.require(:broker_transaction).permit(:price, :ticker, :company, :quantity, :amount, :user_id)
-    end
+    # def set_broker_transaction
+    #   @broker_transaction = @broker_stock.broker_transactions.find(params[:id])
+    # end
+
+    # def broker_stock_params
+    #   params.require(:broker_transaction).permit(:price, :ticker, :company, :quantity, :amount, :user_id, :broker_stock_id)
+    # end
 end
