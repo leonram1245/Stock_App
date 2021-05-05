ae6f5cacc07ab8ef0016a6d7d422dbd9255cef56
Marnoel de Guzman <marnoeldeguzman@gmail.com>
2021-05-05 11:22:27 +0800

    no changes made

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'pending_broker_accounts/index'
   get 'broker_transactions/index'
   get 'buyers_transactions/index'
   get 'broker_stocks/index'
@@ -14,13 +15,10 @@ Rails.application.routes.draw do
   get 'home/search'
   get 'home/about'
   
-  resources :broker_stocks do
-    resources :broker_transactions 
-  end
-  resources :buyers_stocks do
-    resources :buyers_transactions
-  end
+  resources :broker_stocks 
+  resources :broker_transactions 
+  resources :buyers_stocks
+  resources :buyers_transactions
 end
-
 
   
