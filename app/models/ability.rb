7dd5a235539182e7c8b2cc4aabd3539f7bacbcfe
Marnoel de Guzman <marnoeldeguzman@gmail.com>
2021-05-05 11:16:09 +0800

    change management on ability.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,13 +5,16 @@ class Ability
   def initialize(user)
     user ||= User.new 
     if user.has_role? :admin
-      can :manage, :all
+      can :manage, PendingBrokerAccount, :broker_email_confirmed =>false
+      can :manage, ApprovedBrokerAccount, :broker_email_confirmed => true
+      can :read, BrokerStock
       can :access, :rails_admin       # only allow admin users to access Rails Admin
       can :manage, :dashboard         # allow access to dashboard
     elsif user.has_role? :buyer
       can :manage, BuyersStock
+      can :manage, BrokerStock
     elsif user.has_role? :broker
-      can :manage, Stock
+      can :manage, BrokerStock, :broker_email_confirmed =>true
     end
   end
 end
