0d65bfedc4b858a2872437e50256f1b1b3ff77c7
Marnoel de Guzman <marnoeldeguzman@gmail.com>
2021-05-07 23:22:55 +0800

    add required true on password confirmation

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   get 'pending_broker_accounts/index'
   get 'broker_stocks/index'
-  get 'buyers_stocks/available_stocks'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   
   devise_for :users, controllers:  {
