2b3edcd558b6ccef966d0a383e32156c165615fd
Marnoel de Guzman <marnoeldeguzman@gmail.com>
2021-05-08 07:26:15 +0800

    change smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,6 +118,26 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   
+config.action_mailer.delivery_method = :smtp
+host = 'example.com' #replace with your own url
+config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+
+config.action_mailer.perform_deliveries = true
+
+config.action_mailer.raise_delivery_errors = true
+
+config.action_mailer.delivery_method = :smtp
+
+config.action_mailer.smtp_settings = {
+  user_name:     'examplestock1111@gmail.com ',
+  password:      'Password1245',
+  domain:         'gmail.com',
+  address:       'smtp.gmail.com',
+  port:          '587',
+  authentication: :plain,
+  enable_starttls_auto: true
+}
 # SMTP settings for gmail
 
 end
