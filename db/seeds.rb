36a37d88f46c6a3cc3da98751bf66e82f69c1698
Marnoel de Guzman <marnoeldeguzman@gmail.com>
2021-05-08 07:18:11 +0800

    remove smtp settings in production

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 Role.create!(name: 'admin')
 Role.create!(name: 'broker')
 Role.create!(name: 'buyer')
-user = User.create! :email => 'amount@gmail.com', :password => 'secrets', :password_confirmation => 'secrets'
-@user.add_role :admin
-@user.add_role :buyer
+user = User.create! :email => 'amount125151@gmail.com', :password => 'secrets', :password_confirmation => 'secrets'
+user.add_role :admin
+user.add_role :broker
