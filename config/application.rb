57e055edad616ccdfd15a6f07b9865fd86e45025
Marnoel de Guzman <marnoeldeguzman@gmail.com>
2021-05-07 20:31:24 +0800

    remove rails_admin_theme

--- config/application.rb
+++ config/application.rb
@@ -16,11 +16,11 @@ require "sprockets/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-ENV['RAILS_ADMIN_THEME'] = 'rollincode'
 module StockApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.assets.initialize_on_precompile = false
 
     # Configuration for the application, engines, and railties goes here.
     #
