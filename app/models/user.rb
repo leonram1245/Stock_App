dcc5d4ccf0cdbbbaf29489a8d4b2f413e311f65e
Marnoel de Guzman <marnoeldeguzman@gmail.com>
2021-05-08 07:00:52 +0800

    remove confirmable temporarily

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
   rolify
 
   validates :email, :password, :password_confirmation, presence: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
-[:admin, :broker, :buyer].each do |role|
-    Role.find_or_create_by_name({ name: role }, without_protection: true)
-  end
+Role.create!(name: 'admin')
+Role.create!(name: 'broker')
+Role.create!(name: 'buyer')
+user = User.create! :email => 'amount@gmail.com', :password => 'secrets', :password_confirmation => 'secrets'
+@user.add_role :admin
+@user.add_role :buyer
